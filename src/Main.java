d4ca04b1ad5a675b16f20e51aa283c99f70d9ec1
Sofiatrg <sofiat@campus.technion.ac.il>
2023-04-20 20:27:13 +0300

    This is only the base without any changes.

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner;
+    public static Random rnd;
+
+    public static void battleshipGame() {
+        // TODO: Add your code here (and add more methods).
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        scanner = new Scanner(new File(path));
+        int numberOfGames = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Total of " + numberOfGames + " games.");
+
+        for (int i = 1; i <= numberOfGames; i++) {
+            scanner.nextLine();
+            int seed = scanner.nextInt();
+            rnd = new Random(seed);
+            scanner.nextLine();
+            System.out.println("Game number " + i + " starts.");
+            battleshipGame();
+            System.out.println("Game number " + i + " is over.");
+            System.out.println("------------------------------------------------------------");
+        }
+        System.out.println("All games are over.");
+    }
+}
+
+
+
